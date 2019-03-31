0a34691fc08d8a87ee17d7534fd79ece231f6899
iowens2 <iowens2@illinois.edu>
2019-03-31 12:54:17 -0500

    Added profile page and res page

---
+++ app/src/main/java/com/illinois/lavanyap/respg2.java
@@ -0,0 +1,13 @@
+package com.illinois.lavanyap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class respg2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_respg2);
+    }
+}
