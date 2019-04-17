1d1d490b112a2ee575297e2272fe7ec72101522b
Lavanya Piramanayagam <lava281995@gmail.com>
2019-04-16 22:01:35 -0500

    Merge branch 'master' of /Users/lavanyap/AndroidStudioProjects/MyFirstApp with conflicts.

--- app/src/main/java/com/illinois/lavanyap/Login.java
+++ app/src/main/java/com/illinois/lavanyap/Login.java
@@ -1,22 +1,22 @@
 package com.illinois.lavanyap;
 
 import android.app.Activity;
-import android.database.sqlite.SQLiteDatabase;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import android.content.Intent;
 
 public class Login extends Activity {
 
     //private static final android.R.attr R = ;
     private TextView logInTextView_AKA_button;
-    //Cursos resultSet = mydatabase.rawQuery("Select * from Login")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SQLiteDatabase mydatabase = openOrCreateDatabase("enviromovement",MODE_PRIVATE,null);
-        mydatabase.execSQL("CREATE TABLE IF NOT EXISTS Login(UserName VARCHAR, Password VARCHAR);");
-        mydatabase.execSQL("INSERT INTO Login VALUES('ADMIN','ADMIN');");
+
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
 
@@ -30,11 +30,7 @@ public class Login extends Activity {
             }
         });
 
-
-
     }
-
-
 
 
 }
