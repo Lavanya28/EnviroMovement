59d62aee7597f93828300e9b9a60177368db4716
Lavanya Piramanayagam <lava281995@gmail.com>
2019-04-22 19:12:59 -0500

    Edited Resource project pages

--- app/src/main/java/com/illinois/lavanyap/Resource_projectpage.java
+++ app/src/main/java/com/illinois/lavanyap/Resource_projectpage.java
@@ -1,5 +1,8 @@
 package com.illinois.lavanyap;
 
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -11,13 +14,36 @@ public class Resource_projectpage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        Bundle b = this.getIntent().getExtras();
+        String projecttitle = b.getString("project");
+
+        //fetch title of project
+
+        final TextView title = (TextView) findViewById(R.id.title);
+
+        title.setText(projecttitle);
 
         //fetch all textviews representing numbers of resource saved
-        final TextView metric_val = (TextView) findViewById(R.id.tv1);
 
+        final TextView tot = (TextView) findViewById(R.id.total);
 
         final TextView metric = (TextView) findViewById(R.id.title1);
         final TextView motivation = (TextView) findViewById(R.id.title2);
+
+        final SQLiteDatabase mydatabase4 = openOrCreateDatabase("enviromovement",MODE_PRIVATE,null);
+        Cursor cursor = mydatabase4.rawQuery("SELECT * from Project where ProjectName=" +"'"+ projecttitle+"'" ,null);
+
+        cursor.moveToFirst();
+
+        String motivation_text = cursor.getString(3);
+        String metric_text = cursor.getString(4);
+        String total_text = cursor.getString(6);
+
+
+
+        metric.setText(metric_text);
+        motivation.setText(motivation_text);
+        tot.setText(total_text);
 
 
 
